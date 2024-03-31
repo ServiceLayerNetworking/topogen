9dded560dc7e8c95f3ca8e2e16fd73d85889a138
Aditya Prerepa <adiprerepa@gmail.com>
2024-03-31 18:39:28 -0500

    add compute load dimension

--- main.go
+++ main.go
@@ -3,10 +3,46 @@ package main
 import (
 	_ "embed"
 	"flag"
+	"fmt"
 	"github.com/servicelayernetworking/topogen/pkg"
 	"log"
 	"os"
+	"runtime"
+	"time"
 )
+
+func RunCPULoad(millicoreCount int, timeMillis int) {
+	if timeMillis == 0 || millicoreCount == 0 {
+		return
+	}
+
+	percentage := millicoreCount / 10
+	runFor := 1000 * percentage
+	sleepFor := 1000 * (100 - percentage)
+	done := make(chan struct{})
+	go func() {
+		runtime.LockOSThread()
+		// every milliseconds, run for runMicrosecond microseconds, and sleep for sleepMicrosecond microseconds
+		for {
+			select {
+			case <-done:
+				fmt.Printf("Done\n")
+				runtime.UnlockOSThread()
+				return
+			default:
+				begin := time.Now()
+				for {
+					if time.Now().Sub(begin) > time.Duration(runFor)*time.Microsecond {
+						break
+					}
+				}
+				time.Sleep(time.Duration(sleepFor) * time.Microsecond)
+			}
+		}
+	}()
+	time.Sleep(time.Duration(timeMillis) * time.Millisecond)
+	done <- struct{}{}
+}
 
 func main() {
 	// parse command-line arguments for topology file
